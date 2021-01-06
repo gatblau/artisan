d4b13989206d5c6979e5f75853684e153aad141c
gatblau <onix@gatblau.org>
2021-01-06 17:31:50 +0000

    RH Container Tools image

---
+++ runtime/art-rhctools/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# pre-condition health check: all required environment variables have been provided!
+if [[ -z "${ART_REG_USER+x}" ]]; then
+    echo "ART_REG_USER must be provided"
+    exit 1
+fi
+if [[ -z "${ART_REG_PWD+x}" ]]; then
+    echo "ART_REG_PWD must be provided"
+    exit 1
+fi
+if [[ -z "${PACKAGE_NAME+x}" ]]; then
+    echo "PACKAGE_NAME must be provided"
+    exit 1
+fi
+if [[ -z "${PUB_KEY_PATH+x}" ]]; then
+    echo "PUB_KEY_PATH must be provided"
+    exit 1
+fi
+
+# open docker context artefacts
+art exec -u="${ART_REG_USER}:${ART_REG_PWD}" "${PACKAGE_NAME}" build-image -p="${PUB_KEY_PATH}"
---
+++ runtime/art-rhctools/readme.md
@@ -0,0 +1,3 @@
+# Artisan Runtime Image for Red Hat Container Tools
+
+This image contains [buildah](https://github.com/containers/buildah) and [scopeo](https://github.com/containers/skopeo) and can be used to build and sign container images.Artisan
