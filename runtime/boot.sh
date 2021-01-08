d696a82441d45c8b5e8c8895433e99b07f172d3f
gatblau <onix@gatblau.org>
2021-01-08 08:17:13 +0000

    generic bootstrap for all runtimes

---
+++ runtime/boot.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#  Onix Config Manager - Bootstrap for Runtime Image
+#  Copyright (c) 2018-2021 by www.gatblau.org
+#  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Contributors to this project, hereby assign copyright in this code to the project,
+#  to be licensed under the same terms as the rest of the code.
+#
+if [[ -z "${PACKAGE_NAME+x}" ]]; then
+  if [[ -z "${PUB_KEY_FILE+x}" ]]; then
+    echo "A full path to the PGP public key used to open the package is required: PUB_KEY_FILE must be provided"
+    exit 1
+  fi
+  # if a user name is defined
+  if [[ -z "${ART_REG_USER+x}" ]]; then
+    # then requires a corresponding password
+    if [[ -z "${ART_REG_PWD+x}" ]]; then
+      echo "The password for the Artisan Registry user is required: ART_REG_PWD must be provided"
+      exit 1
+    fi
+  fi
+  # if a function has been defined executes the package with the function
+  if [[ -z "${FX_NAME+x}" ]]; then
+      # pass the function name
+      art exec -u=${ART_REG_USER}:${ART_REG_PWD} ${PACKAGE_NAME} ${FX_NAME} -p=${PUB_KEY_FILE}
+  else
+      # executes the default function
+      art exec -u=${ART_REG_USER}:${ART_REG_PWD} ${PACKAGE_NAME} -p=${PUB_KEY_FILE}
+  fi
+  # else if only a function has been defined
+elif [[ -z "${FX_NAME+x}" ]]; then
+  # run the function from the build.yaml in the mounted source
+  art run ${FX_NAME}
+else
+  # no package and no function have been defined then run the runtime bootstrapping script
+  sh run.sh
+fi
