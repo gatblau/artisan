6c18e9551cb24101be4eaa6091c130386c88592e
gatblau <onix@gatblau.org>
2021-01-29 12:31:59 +0000

    update runtime

--- runtime/sonar/run.sh
+++ runtime/sonar/run.sh
@@ -26,6 +26,9 @@ fi
 # merge sonar URI variable
 printf "sonar.sourceEncoding=UTF-8\nsonar.host.url=%s\n" "${SONAR_URI}" > /sonar/conf/sonar-scanner.properties
 
+# take the start time for the scanning process
+START_TIME=$(date +"%Y-%m-%dT%T+0000")
+
 # initiates the scan process
 sonar-scanner -Dsonar.projectKey="${SONAR_PROJECT_KEY}" -Dsonar.sources="${SONAR_SOURCES}" -Dsonar.java.binaries="${SONAR_BINARIES}" -Dsonar.login="${SONAR_TOKEN}"
 
@@ -37,22 +40,7 @@ fi
 
 printf "sonar server is %s\n" "${SONAR_URI}"
 printf "project key is %s\n" "${SONAR_PROJECT_KEY}"
+printf "waiting for analysis to complete"
 
-# retrieve analysis identifier from Sonar
-analysis_id=$(curl -s "${SONAR_URI}/api/project_analyses/search?project=${SONAR_PROJECT_KEY}" | jq '.analyses[]' | jq '.key' | head -n 1)
-analysis_id=$(echo "${analysis_id}" | tr -d "\"\`'")
-
-printf "analysis is %s\n" "${analysis_id}"
-
-# using the identifier retrieves the analysis status
-analysis_status=$(curl -s "${SONAR_URI}/api/qualitygates/project_status?analysisId=${analysis_id}" | jq '.projectStatus.status')
-
-printf "analysis status is %s\n" "${analysis_status}"
-
-if [[ "${analysis_status}" = \""OK\"" ]]; then
-  printf "Sonar Code Quality check => Pass\n"
-  exit 0
-fi
-
-printf "Sonar Code Quality check => Failed\n"
-exit 1
+# query sonar API until the uploaded report has been analysed
+art wait -f "$.analyses[?(@.date>'${START_TIME}')].date" -a 20 "${SONAR_URI}/api/project_analyses/search?project=${SONAR_PROJECT_KEY}"
