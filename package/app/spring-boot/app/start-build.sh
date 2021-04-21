4b064cced807f8701ae693e2d612b31aefbdcf5f
pratik10596 <pratikpradhan10596@gmail.com>
2021-04-21 20:35:10 +0530

    added spring-boot package & fix uid/gid ranges

---
+++ package/app/spring-boot/app/start-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# consuming environment variables which are set by the build-image function
+art merge buildconfig.yaml.tem
+if [ "$?" -ne 0 ]; then
+  echo "failed to merge buildconfig.yaml.tem"
+  exit 1
+fi
+
+oc create -f buildconfig.yaml
+
+# run custom build
+oc start-build ${BC_APPLICATION_NAME}-custom-build -F | tee buildconfig.log
+
+# delete custom build from openshift
+oc delete -f buildconfig.yaml
+
+# check for errors before leaving the pipeline
+# validating success & error scenarios
+success_count=$(grep -c COMMIT buildconfig.log)
+error_count=$(grep -c error buildconfig.log)
+if [ $error_count -gt 0 ] || [ $success_count -eq 0 ]; then
+  echo "Failed to build an image"
+  exit 1
+fi
---
+++ package/app/spring-boot/setup/readme.md
@@ -0,0 +1,21 @@
+# Spring-Boot Application Setup
+
+This folder contains all of the assets to setup a Spring-Boot application project.
+
+## Create Spring-Boot Scaffolding
+
+```bash
+art runc init-mvn
+```
+
+## Setup Git
+
+```bash
+art runc init-git
+```
+
+## Setup Tekton CI pipeline
+
+```bash
+art runc init-tkn
+```
