90063bb577b3ab3503275eb18fe1d65416af56a4
pratik10596 <pratikpradhan10596@gmail.com>
2021-05-12 06:57:09 +0000

    change home directory fot int-git

--- package/app/quarkus/setup/setup-git.sh
+++ package/app/quarkus/setup/setup-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Store git Credentials
-cat > ${HOME}/.netrc <<EOF
+cat > ${PIPELINE_HOME}/.netrc <<EOF
 
 machine $1
 login $2
@@ -11,8 +11,8 @@ EOF
 
 cd app
 git init
-git config --global user.email "${GIT_REPO_EMAIL}"
-git config --global user.name "${GIT_REPO_UNAME}"
+git config user.email "${GIT_REPO_EMAIL}"
+git config user.name "${GIT_REPO_UNAME}"
 git add .
 git commit -m "first commit"
 git remote add origin ${GIT_URI}
